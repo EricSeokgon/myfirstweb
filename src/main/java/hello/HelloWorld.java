f141d3d651612c97948aa67505f6b36e1925ea2c
sklee <hadesleeq@gmail.com>
2017-02-27 10:35:13 +0900

    hello world test

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * Project: myfirstweb
+ * FileName: HelloWorld
+ * Date: 2017-02-27
+ * Time: 오전 10:25
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public String message() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/hello/HelloWorldTest.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Project: myfirstweb
+ * FileName: HelloWorldTest
+ * Date: 2017-02-27
+ * Time: 오전 10:29
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorldTest {
+    @Test
+    public void message() throws Exception {
+        HelloWorld hw = new HelloWorld();
+        assertEquals("Hello World", hw.message());
+    }
+
+}
